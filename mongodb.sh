0810e31ad85239efd8401c8876538d171272d06c
DevOpsProCloud <git-repo@devopsprocloud.in>
2025-04-03 23:54:54 +0530

    example-1

---
+++ mongodb.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%H-%S)
+LOGFILE=/tmp/$@-$TIMESTAMP.log
+
+echo -e "$Y Script started executing at $TIMESTAMP $N."
+
+VALIDATE() {
+    if  [ $1 -ne 0 ]
+    then 
+        echo -e "$2...$R FAILED $N."
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e " $R ERROR $N You are not a root user"
+    exit 1
+else
+fi
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+VALIDATE $? "Copying Mongo Repo"
+
+dnf install mongodb-org -y &>> $LOGFILE
+VALIDATE $? "Installing MongoDB"
+
+systemctl enable mongod &>> $LOGFILE
+VALIDATE $? "Enabling MongoDB"
+
+systemctl start mongod &>> $LOGFILE
+VALIDATE $? "Starting MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
+VALIDATE $? "Allowing Connections to MongoDB"
+
+systemctl restart mongod &>> $LOGFILE
+VALIDATE $? "Restarting MongoDB"
