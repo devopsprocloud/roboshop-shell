c7aabf72a66548268fb0d033c97757b66d7f0355
DevOpsProCloud <git-repo@devopsprocloud.in>
2025-04-04 09:03:52 +0530

    rabbitmq

---
+++ rabbitmq.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%H-%S)
+LOGFILE=/tmp/$@-$TIMESTAMP.log
+
+echo -e "$Y Script started executing at $TIMESTAMP $N."
+
+VALIDATE() {
+    if  [ $1 -ne 0 ]
+    then 
+        echo -e "$2...$R FAILED $N."
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e " $R ERROR $N You are not a root user"
+    exit 1
+fi
+
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>> $LOGFILE
+VALIDATE $? "Downloading earlandg script" 
+
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>> $LOGFILE
+VALIDATE $? "Configuring RabbitMQ Repo" 
+
+dnf install rabbitmq-server -y &>> $LOGFILE
+VALIDATE $? "Installing RabbitMQ" 
+
+systemctl enable rabbitmq-server &>> $LOGFILE
+VALIDATE $? "Enabling RabbitMQ" 
+
+systemctl start rabbitmq-server &>> $LOGFILE
+VALIDATE $? "Starting RabbitMQ" 
+
+rabbitmqctl add_user roboshop roboshop123 &>> $LOGFILE
+VALIDATE $? "Setting Up the default USERNAME and PASSWORD" 
+
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> $LOGFILE
+VALIDATE $? "Setting Up the permissions for 'roboshop' user" 
+
+
+
+
+
